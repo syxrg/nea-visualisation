381c5e37f1b0bba1e81cf8bdae5bbb96ab9d6b5d
Serena Gong <sxgong2@illinois.edu>
2022-02-01 23:04:23 -0600

    Added first test functions to JSONTest

--- src/test/com/example/JSONTest.java
+++ src/test/com/example/JSONTest.java
@@ -1,6 +1,7 @@
 package com.example;
 
 
+import com.example.SatelliteDataProcessing;
 import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,5 +16,17 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class JSONTest {
+    private SatelliteDataProcessing satelliteData;
+    @Before
+    public void setUp() throws IOException {
+        Gson gson = new Gson();
+        Reader jsonReader = Files.newBufferedReader(Paths.get("src/main/resources/satellite.json"));
+        satelliteData = gson.fromJson(jsonReader, SatelliteDataProcessing.class);
+    }
+
+    @Test
+    public void checkCorrectNumberOfSatellitesInDataSet() {
+        assertEquals(50, satelliteData.getSatelliteDataList().size());
+    }
 
 }

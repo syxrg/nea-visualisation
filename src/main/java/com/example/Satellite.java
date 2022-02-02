2aa8929ba72104c9c237e304cfcb5cf9a037a9e7
Serena Gong <sxgong2@illinois.edu>
2022-02-01 21:26:51 -0600

    Created class Satellite.java

--- src/main/java/com/example/Satellite.java
+++ src/main/java/com/example/Satellite.java
@@ -4,7 +4,9 @@ public class Satellite {
     private int id;
     private int planetId;
     private String name;
-    private String gm;
-
+    private String radius;
+    private String density;
+    private String magnitude;
+    private String albedo;
 
 }

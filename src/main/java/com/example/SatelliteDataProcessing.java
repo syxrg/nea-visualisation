9b5ac8fe61cacaf7378c42c1087ffc8c062014d2
Serena Gong <sxgong2@illinois.edu>
2022-02-01 22:07:21 -0600

    Updated SatelliteDataProcessing with Serialisation

--- src/main/java/com/example/SatelliteDataProcessing.java
+++ src/main/java/com/example/SatelliteDataProcessing.java
@@ -1,4 +1,16 @@
 package com.example;
 
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+
 public class SatelliteDataProcessing {
+    @SerializedName("satellites")
+
+    private List<Satellite> satelliteDataList;
+    public List<Satellite> getSatelliteDataList() {
+        return satelliteDataList;
+    }
+
+    
 }
